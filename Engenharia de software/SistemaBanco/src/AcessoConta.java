62a1b865de302434b79871638f56f6edf63dcb33
CarlosTorres2305 <carlosadm15@hotmail.com>
2022-09-26 19:14:01 -0300

    modificações no código

---
+++ Engenharia de software/SistemaBanco/src/AcessoConta.java
@@ -0,0 +1,24 @@
+
+//Area de testes
+public class AcessoConta {
+
+	public static void main(String[] args) {
+		Conta c1 = new Conta("Carlos",2323,"1234");
+		Conta c2 = new Conta("Maria",2304,"5678");
+		
+		CaixaEletronico cx1 = new CaixaEletronico();
+		
+		cx1.acessarConta("1234", c1);
+		cx1.efetuarDeposito(5000, c1);
+		cx1.consultarSaldo(c1);
+		cx1.efetuarTransferencia(300, c1, c2);
+		
+		cx1.acessarConta("5678", c2);
+		cx1.efetuarSaque(100, c2);
+		
+		
+		
+
+	}
+
+}
--- Engenharia de software/SistemaBanco/src/CaixaEletronico.java
+++ Engenharia de software/SistemaBanco/src/CaixaEletronico.java
@@ -5,46 +5,57 @@ public class CaixaEletronico {
 	private float dinheiro;
 	
 	//Métodos personalizados
-	public void efetuarSaque(float valor){
-		if(this.getDinheiro() > valor) {			
-			if(this.getConta().getSaldo() < valor) {
-				System.out.println("Saldo insuficiente!");
-			} else {
-				System.out.println("Saque de: " + valor + "realizado com sucesso");
-				valor = this.getConta().getSaldo() - valor;
-				this.getConta().setSaldo(valor);
-			}
-		}else {
-			System.out.println("Impossível sacar faltam cédulas!");
+	public void efetuarSaque(float valor, Conta acesso){
+		this.setConta(acesso);
+		if(this.getConta().getAberta()) {
+			if(this.getDinheiro() > valor) {			
+				if(this.getConta().getSaldo() < valor) {
+					System.out.println("Saldo insuficiente!");
+				} else {
+					System.out.println("Saque de: " + valor + " realizado com sucesso");
+					valor = this.getConta().getSaldo() - valor;
+					this.getConta().setSaldo(valor);
+					System.out.println("Novo saldo de: " + this.getConta().getCliente() + " R$"+ this.getConta().getSaldo());
+				}
+				} else {
+					System.out.println("Impossível sacar faltam cédulas!");
+				}
+		} else {
+			System.out.println("Acesso negado!");
 		}
 	}
-	public void efetuarDeposito(float valor){
+	public void efetuarDeposito(float valor, Conta acesso){
+		this.setConta(acesso);
 		if (this.getConta().getAberta()) {
 			this.getConta().setSaldo(this.getConta().getSaldo() + valor);
 			System.out.println("Depósito de: R$" + valor + " realizado com sucesso!");
-			this.getConta().mostrarStatus();
 		} else {
 			System.out.println("Acesso Negado!");
 		}
 	}
-	public void consultarSaldo() {
+	public void consultarSaldo(Conta acesso) {
+		this.setConta(acesso);
 		if (this.getConta().getAberta()) {
 			System.out.println("SALDO: R$" + this.getConta().getSaldo());
 		} else {
 			System.out.println("Acesso Negado!");
 		}
 	}
-	public void efetuarTransferencia(float valor, Conta para){
+	public void efetuarTransferencia(float valor,Conta acesso,Conta para){
+		this.setConta(acesso);
 		if (this.getConta().getAberta()) {
 			if (this.getConta().getSaldo() > valor) {
 				para.setSaldo(para.getSaldo() + valor);
 				this.getConta().setSaldo(this.getConta().getSaldo() - valor);
+				System.out.println("Transferência de R$" + valor + " realizada com sucesso!"); 
+				System.out.println("Novo saldo de: " + this.getConta().getCliente() + " R$"+ this.getConta().getSaldo());
 			}
 		} else {
 			System.out.println("Acesso Negado!");
 		}
 	}
-	public void acessarConta(String password){
+	public void acessarConta(String password, Conta acesso){
+		this.setConta(acesso);
 		if (password == this.getConta().getSenha()) {
 			this.getConta().setAberta(true);
 			System.out.println("Seja Bem vindo: " + this.getConta().getCliente());
@@ -61,6 +72,7 @@ public class CaixaEletronico {
 	//Métodos especiais
 	public CaixaEletronico(){
 		this.setDinheiro(2000);
+	}	
 	public Conta getConta() {
 		return conta;
 	}
